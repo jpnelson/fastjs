1d3c6a5d53bcbdebd29f56b70aba6a752bf30446
Joshua Nelson <jonelson@atlassian.com>
2018-06-01 00:28:45 -0700

    Add caching

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
 const express = require("express");
 const app = express();
 
-var request = require("request");
+const request = require("request");
+const NodeCache = require("node-cache");
+const cache = new NodeCache({ stdTTL: 60 });
 
 app.get("/", (req, res) => {
   const subdomains = req.subdomains;
-  console.log("Headers", JSON.stringify(req.headers));
-  console.log("Subdomains", subdomains);
   if (subdomains.length === 0) {
-    res.sendStatus(404);
+    res.redirect("fastjs.link");
     return;
   }
   if (subdomains.length === 1) {
     const package = subdomains[0];
-    request(
-      `https://registry.npmjs.org/${package}/latest`,
-      (err, response, body) => {
-        if (err) {
-          res.send(500);
-          return;
-        }
 
-        const json = JSON.parse(body);
-
-        const homepage =
-          json.homepage || `https://www.npmjs.com/package/${package}`;
-
-        res.redirect(json.homepage);
+    cache.get(package, (cacheErr, cacheData) => {
+      if (!cacheErr && cacheData) {
+        res.send(cacheData);
         return;
       }
-    );
+      request(
+        `https://registry.npmjs.org/${package}/latest`,
+        (err, response, body) => {
+          if (err) {
+            res.send(500);
+            return;
+          }
+
+          const json = JSON.parse(body);
+
+          const homepage =
+            json.homepage || `https://www.npmjs.com/package/${package}`;
+
+          cache.set(package, homepage, () => {
+            res.redirect(homepage);
+            return;
+          });
+        }
+      );
+    });
   } else {
     res.sendStatus(404);
     return;
